68d910e80614eea40e5ea27692baa77ec0412146
Aldo Aldiero <alexander@privyid.tech>
2023-03-15 19:13:27 +0700

    fix: responsiveness

--- components/Organism/OMovieList.vue
+++ components/Organism/OMovieList.vue
@@ -3,7 +3,7 @@
     <ACard class="w-full">
       <MSearchbar v-model="search" />
     </ACard>
-    <div class="grid grid-cols-4 gap-4">
+    <div class="grid grid-cols-1 gap-4 md:grid-cols-4 sm:grid-cols-2">
       <MMovieCards
         v-for="movie in data?.results"
         :title="movie.title"
@@ -19,7 +19,7 @@
 import ACard from "../Atoms/ACard.vue";
 import MSearchbar from "~~/components/Molecules/MSearchbar.vue";
 import MMovieCards from "../Molecules/MMovieCards.vue";
-import { ref, reactive } from "vue";
+import { ref } from "vue";
 import { IData, IMovie } from "@/models/movies";
 
 const search = ref("");
@@ -27,7 +27,7 @@ const search = ref("");
 const config = useRuntimeConfig();
 const api_key: string = config.API_KEY;
 const api_base_url: string = config.API_BASE_URL;
-const api_base_image_url: string = config.API_BASE_IMG_URL;
+// const api_base_image_url: string = config.API_BASE_IMG_URL;
 const { data: data } = useFetch<IData<IMovie>>(
   `${api_base_url}/movie/popular`,
   {

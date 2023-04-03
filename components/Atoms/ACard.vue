1e0b4f0ff0e94a9a5c75b5660e0ba2186c0c1859
Aldo Aldiero <alexander@privyid.tech>
2023-04-03 14:11:25 +0700

    fix: card shadow

--- components/Atoms/ACard.vue
+++ components/Atoms/ACard.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="rounded border border-gray-50 shadow-grey-100 bg-white"
+    class="rounded border border-gray-50 shadow bg-white"
     :class="{
       'w-fit h-fit': fit,
     }"
